8a195807a8a3e8a34b004760a8c3e01f2155bd8e
Stephan Osthold <osthold@modell-aachen.de>
2014-06-24 11:21:33 +0200

    Do not merge: Grinding
    Not for production use!

---
+++ lib/Foswiki/Plugins/SolrPlugin/Grinder.pm
@@ -0,0 +1,50 @@
+use strict;
+use warnings;
+
+{
+    handle_message => sub {
+        my ($host, $t, $hdl, $run_engine, $json) = @_;
+        if ($t =~ m'update_topic|update_web') {
+            $main::mattworker_data{caches} = $json->{cache};
+            eval { $run_engine->(); };
+            if ($@) {
+                print "Worker: $t exception: $@\n";
+            } else {
+                for my $c ('groups_members', 'web_acls') {
+                    $hdl->push_write(json => {
+                        type => 'set_cache',
+                        host => $host,
+                        data => {key => $c, value => $main::mattworker_data{caches}{$c}},
+                    });
+                }
+            }
+        } elsif ($t eq 'flush_acls') {
+            print "Flush web ACL cache\n";
+            $hdl->push_write(json => {type => 'clear_cache', host => $host});
+        } elsif ($t eq 'flush_groups') {
+            print "Flush group membership cache\n";
+            $hdl->push_write(json => {type => 'clear_cache', host => $host});
+        }
+    },
+    engine_part => sub {
+        my ($session, $type, $data, $caches) = @_;
+        my $indexer = Foswiki::Plugins::SolrPlugin::getIndexer($session);
+        $indexer->groupsCache($caches->{groups_members}) if $caches->{groups_members};
+        $indexer->webACLsCache($caches->{web_acls}) if $caches->{web_acls};
+
+        if ($type eq 'update_topic') {
+            $indexer->updateTopic(undef, $data);
+            $indexer->commit(1);
+        }
+        elsif ($type eq 'update_web') {
+            $indexer->update($data);
+            $indexer->commit(1);
+        }
+
+        $main::mattworker_data{caches} = {
+            groups_members => $indexer->groupsCache(),
+            web_acls => $indexer->webACLsCache(),
+        };
+
+    },
+};
