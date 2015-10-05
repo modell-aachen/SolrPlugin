257fa0403b3ede67e532bd6776b68a7b275e427b
Stephan Osthold <osthold@modell-aachen.de>
2015-10-05 11:42:21 +0200

    Webmappings: Try translations of whole web first; better compatibility
    Fallback to translating each part individually.
    
    Using jQuery's each, for better compatibility (quirks mode).
    
    Normalizing the web deliminators to slashed.
    
    Ticket: none
    No release-note: fix to unreleased feature
    Release-Note: none

--- pub/System/SolrPlugin/ajax-solr-widgets.uncompressed.js
+++ pub/System/SolrPlugin/ajax-solr-widgets.uncompressed.js
@@ -3,8 +3,18 @@
 
   AjaxSolr.getWebMapping = function(web) {
       if(!AjaxSolr.Dicts.webmappings) return web;
+
+      web = web.replace(/\./g, '/');
+
+      // first, lets see if there is a mapping for the full path
+      var mapped = AjaxSolr.Dicts.webmappings.get(web);
+      if(mapped !== web) return mapped;
+
+      // we did not find a mapping, or it is equal (no way to catch that)
+      // lets try each part individually
+      // XXX We should try combinations first eg. web='a/b/c' -> webmappings.get('a' + '/' + 'b') + '/' +webmappings('c')
       var mappings = [];
-      web.split(/\./).forEach(function(eachWeb, idx){
+      $.each(web.split(/\//), function(idx, eachWeb){
           mappings[idx] = AjaxSolr.Dicts.webmappings.get(eachWeb);
       });
       return mappings.join('/');
